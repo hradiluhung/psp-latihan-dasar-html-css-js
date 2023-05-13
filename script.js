aa2e29f08fbf606f5da985c67af0e2777b5f9a4c
Hakam Royhan Adiluhung <hakamra107@gmail.com>
2023-05-13 08:01:13 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const counter = document.querySelector(".value");
+const buttonAdd = document.querySelector(".button-add");
+const buttonSubstract = document.querySelector(".button-substract");
+
+let count = 0;
+
+buttonAdd.addEventListener("click", function () {
+  // TODO 1: Tambahkan event listener untuk buttonAdd
+});
+
+buttonSubstract.addEventListener("click", function () {
+  // TODO 2: Tambahkan event listener untuk buttonSubstract
+});
